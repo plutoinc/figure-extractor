69f558c284904a4c5b0a56c60763bb36ede62d5c
Tylor Shin <shincode@icloud.com>
2018-11-07 16:12:20 +0900

    Add updating DynamoDB logic

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,16 @@ import { extractImgs } from "./extractImgs";
 import { getImgFileNames } from "./getImgFilenames";
 import { cleanSmallByteImages } from "./cleanImgs";
 import { uploadFolder } from "./uploadToS3";
+import { updateDynamoDB } from "./updateDynamoDB";
 
-type ProgressStatus = "not_started" | "pending" | "done";
+export type ProgressStatus = "not_started" | "pending" | "done";
 
 export interface MessageBody {
   paper_id: string;
   paper_urls: string[];
 }
 
-interface Paper {
+export interface Paper {
   paperId: string;
   paperUrls: string[];
   paperImages: string[];
@@ -47,10 +48,11 @@ setInterval(() => {
               const message = JSON.parse(msg.Body) as MessageBody;
 
               const pdfPath = await downloadPDF(msg.MessageId, message);
+
               if (!pdfPath || pdfPath.length === 0) {
                 console.log("There isn't any PDF file to extract");
                 if (msg.ReceiptHandle) {
-                  deleteMessage(msg.ReceiptHandle);
+                  return deleteMessage(msg.ReceiptHandle);
                 }
               }
 
@@ -60,22 +62,22 @@ setInterval(() => {
 
               if (imgFilenames && imgFilenames.length > 0) {
                 cleanSmallByteImages(imgFilenames, dirForPdfImg);
-                const s3Keys = await uploadFolder(
-                  dirForPdfImg,
-                  message.paper_id
-                );
-                const paperImages = s3Keys.filter(key => key.endsWith(".png"));
-                const paperPdf = s3Keys.find(key => key.endsWith(".pdf")) || "";
-
-                const paper: Paper = {
-                  paperId: message.paper_id,
-                  paperUrls: message.paper_urls,
-                  paperImages,
-                  paperPdf,
-                  status: "done"
-                };
-                console.log(paper);
               }
+
+              const s3Keys = await uploadFolder(dirForPdfImg, message.paper_id);
+              const paperImages =
+                s3Keys.filter(key => key.endsWith(".png")) || [];
+              const paperPdf = s3Keys.find(key => key.endsWith(".pdf")) || "";
+
+              const paper: Paper = {
+                paperId: message.paper_id,
+                paperUrls: message.paper_urls,
+                paperImages,
+                paperPdf,
+                status: "done"
+              };
+
+              await updateDynamoDB(paper);
             } catch (err) {
               console.error(err);
             }
---
+++ src/updateDynamoDB.ts
@@ -0,0 +1,56 @@
+import * as AWS from "aws-sdk";
+import { Paper, ProgressStatus } from "./";
+import { PutItemInputAttributeMap } from "aws-sdk/clients/dynamodb";
+
+const dynamodb = new AWS.DynamoDB();
+const TABLE_NAME = "paper-pdf-extarct";
+
+interface DynamoDBPaperParams extends PutItemInputAttributeMap {
+  PaperId: {
+    S: string;
+  };
+  paperUrls: {
+    SS: string[];
+  };
+  paperImages: {
+    SS: string[];
+  };
+  paperPdf: {
+    S: string;
+  };
+  status: {
+    S: ProgressStatus;
+  };
+}
+
+export async function updateDynamoDB(paper: Paper) {
+  const item: DynamoDBPaperParams = {
+    PaperId: {
+      S: paper.paperId
+    },
+    paperUrls: {
+      SS: paper.paperUrls
+    },
+    paperImages: {
+      SS: paper.paperImages
+    },
+    paperPdf: {
+      S: paper.paperPdf
+    },
+    status: {
+      S: paper.status
+    }
+  };
+
+  const params = {
+    Item: item,
+    TableName: TABLE_NAME
+  };
+
+  try {
+    await dynamodb.putItem(params).promise();
+  } catch (err) {
+    console.error("ERROR OCCURRED AT UPDATE DYNAMO DB");
+    throw new Error(err);
+  }
+}
